39229ada4fe0c723fc2e79b8d530d0ddfb9d61ab
HoKirby <lakevely27@gmail.com>
2025-01-14 00:20:03 +0900

    feat/chore: Kiosk 프로그램 Lv. 5 구현
    private 키워드와 setter를 이용해 메뉴 Collection 캡슐화
    가독성을 위해 주석 추가 및 코드 줄바꿈 수정

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -2,12 +2,13 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kiosk {
-    Menu menu;
+    private final Menu menu; //캡슐화
 
     Kiosk(Menu menu) {
         this.menu = menu;
     }
 
+    //키오스크 프로그램 실행
     public void start() {
         Scanner sc = new Scanner(System.in);
         while (true) {
@@ -17,7 +18,7 @@ public class Kiosk {
             int categoryNum;
             try {
                 categoryNum = sc.nextInt();
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) { //입력 정보가 int 타입이 아닐경우 예외처리
                 OrderLogger.logger.info("Invalid Input");
                 sc.nextLine();
                 continue;
@@ -26,7 +27,7 @@ public class Kiosk {
                 OrderLogger.logger.info("프로그램을 종료합니다.");
                 System.exit(0);
             }
-            if (menu.getMenuItems().length < categoryNum) {
+            if (menu.getMenuItems().length < categoryNum) { //입력 정보가 존재하는 카테고리 수 보다 큰 수일 경우 예외처리
                 OrderLogger.logger.info("Invalid Number");
                 continue;
             }
--- src/Menu.java
+++ src/Menu.java
@@ -3,8 +3,9 @@ import MenuItems.MenuItem;
 import java.util.ArrayList;
 
 public class Menu {
-    ArrayList<MenuItem>[] menuItems;
+    private ArrayList<MenuItem>[] menuItems; //캡슐화
 
+    //메뉴 카테고리 수 설정
     public void setMenuCategories(int categoryNum) {
         menuItems = new ArrayList[categoryNum];
         for (int i = 0; i < categoryNum; i++) {
@@ -12,10 +13,16 @@ public class Menu {
         }
     }
 
+    //리스트에 카테고리별 메뉴 객체 추가
+    public void setMenuItems(int categoryNum, MenuItem menuItem) {
+        menuItems[categoryNum].add(menuItem);
+    }
+
     public ArrayList<MenuItem>[] getMenuItems() {
         return menuItems;
     }
 
+    //콘솔창에 카테고리 출력
     public void logCategories() {
         int i = 1;
         for (ArrayList<MenuItem> list : menuItems) {
@@ -24,15 +31,21 @@ public class Menu {
         }
     }
 
+    //콘솔창에 카테고리 정보 출력
     public void logMenuItems(int categoryNum) {
-        OrderLogger.logger.info("[ " + menuItems[categoryNum-1].get(0).getClass().toString().split("\\.")[1] + " MENU ]");
-        for(int i = 0; i < menuItems[categoryNum -1].size(); i++) {
-           OrderLogger.logger.info((i + 1) + ". " + menuItems[categoryNum-1].get(i).getName() + " | W " + menuItems[categoryNum-1].get(i).getPrice() + " | " + menuItems[categoryNum-1].get(i).getInfo());
+        OrderLogger.logger.info("[ " + menuItems[categoryNum - 1].get(0).getClass().toString().split("\\.")[1] + " MENU ]");
+        for (int i = 0; i < menuItems[categoryNum - 1].size(); i++) {
+            OrderLogger.logger.info((i + 1) + ". " + menuItems[categoryNum - 1].get(i).getName()
+                    + " | W " + menuItems[categoryNum - 1].get(i).getPrice()
+                    + " | " + menuItems[categoryNum - 1].get(i).getInfo());
         }
     }
 
+    //콘솔창에 카테고리에 해당하는 메뉴 정보 출력
     public void logMenu(int categoryNum, int orderNum) {
-        OrderLogger.logger.info("선택한 메뉴: " + menuItems[categoryNum - 1].get(orderNum - 1).getName() + " | W " + menuItems[categoryNum - 1].get(orderNum - 1).getPrice() + " | " + menuItems[categoryNum - 1].get(orderNum - 1).getInfo());
+        OrderLogger.logger.info("선택한 메뉴: " + menuItems[categoryNum - 1].get(orderNum - 1).getName()
+                + " | W " + menuItems[categoryNum - 1].get(orderNum - 1).getPrice()
+                + " | " + menuItems[categoryNum - 1].get(orderNum - 1).getInfo());
     }
 
 }
